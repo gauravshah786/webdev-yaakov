b7a19832f0a86af1f1b1b2f4a9eeb4f6b8009ad2
gauravshah786 <gauravshah_786@yahoo.com>
2017-02-11 15:28:45 -0600

    Add files via upload

---
+++ module3-solution/app.js
@@ -0,0 +1,88 @@
+(function () {
+'use strict';
+
+	angular.module('NarrowItDownApp',[])
+	.controller('NarrowItDownController',NarrowItDownController)
+	.service('MenuSearchService',MenuSearchService)
+	.directive('foundItems', FoundItemsDirective)
+	.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
+
+	function FoundItemsDirective() {
+	  var ddo = {
+		templateUrl: 'searchResult.html',
+		restrict: 'E',
+		scope: {
+		  found: '<',
+		  message: '@',
+		  onRemove: '&'
+		},
+		controller: NarrowItDownController,
+		controllerAs: 'narrowItDown',
+		bindToController: true
+	  };
+	  return ddo;
+	}
+
+	NarrowItDownController.$inject = ['MenuSearchService'];
+	function NarrowItDownController(MenuSearchService) {
+	  var menu = this;
+	  menu.found = [];
+	  menu.message = "";
+	  
+	  menu.searchMenu = function (searchTerm) {
+		var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
+
+		promise.then(function (response) {
+			if(response != null && response.length > 0){
+				menu.found = MenuSearchService.getItems();
+				menu.message = "Success";
+			}
+			else if(response.length == 0)
+				menu.message = "Nothing Found";
+		})
+		.catch(function (error) {
+		  console.log(error);
+		})
+		
+	  };
+	  
+	  menu.removeItem = function (itemIndex) {
+		MenuSearchService.removeItem(itemIndex);
+	  };
+	}
+
+	MenuSearchService.$inject = ['$http', 'ApiBasePath'];
+	function MenuSearchService($http, ApiBasePath) {
+		var service = this;
+		var foundItems = [];
+		
+		service.getMatchedMenuItems = function (searchTerm) {
+			return $http({
+				method: "GET",
+				url: (ApiBasePath + "/menu_items.json")
+			})
+			.then(function (result) {
+				foundItems = [];
+				result.data.menu_items.forEach(function(obj) {
+					if(obj.description.includes(searchTerm)){
+						var tempObj = {
+							name: obj.name,
+							short_name: obj.short_name,
+							description: obj.description
+						};
+						foundItems.push(tempObj); 
+					}
+				});
+				return foundItems;
+			});
+		};
+	  
+		service.getItems = function (){
+		  return foundItems;
+		};
+	  
+		service.removeItem = function (itemIndex) {
+			foundItems.splice(itemIndex, 1);
+		};
+	}
+})();
