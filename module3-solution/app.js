7be72695ab03b3f6b8060420fe8acfa7423bfd59
nisevi <nicolas.s.vidal@gmail.com>
2017-02-12 14:41:13 +0100

    Fixed 'not found' functionality.

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -13,39 +13,38 @@
 		restrict: 'E',
 		scope: {
 		  found: '<',
-		  message: '@',
-		  onRemove: '&'
+		  onRemove: '&',
+			empty: '<'
 		},
-		controller: NarrowItDownController,
+		controller: FoundItemsDirectiveController,
 		controllerAs: 'narrowItDown',
 		bindToController: true
 	  };
 	  return ddo;
 	}
 
+	function FoundItemsDirectiveController(){
+		var list = this;
+	}
+
 	NarrowItDownController.$inject = ['MenuSearchService'];
 	function NarrowItDownController(MenuSearchService) {
 	  var menu = this;
+		menu.searchTerm = "";
 	  menu.found = [];
-	  menu.message = "";
-	  
-	  menu.searchMenu = function (searchTerm) {
-		var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
+		menu.empty = false;
 
-		promise.then(function (response) {
-			if(response != null && response.length > 0){
+	  menu.searchMenu = function (searchTerm) {
+			var promise = MenuSearchService.getMatchedMenuItems(menu.searchTerm);
+			promise.then(function (response) {
 				menu.found = response;
-				menu.message = "Success";
-			}
-			else if(response.length == 0)
-				menu.message = "Nothing Found";
-		})
-		.catch(function (error) {
-		  console.log(error);
-		})
-		
+				menu.empty = MenuSearchService.isEmpty();
+			})
+			.catch(function (error) {
+			  console.log(error);
+			})
 	  };
-	  
+
 	  menu.removeItem = function (itemIndex) {
 		MenuSearchService.removeItem(itemIndex);
 	  };
@@ -55,30 +54,41 @@
 	function MenuSearchService($http, ApiBasePath) {
 		var service = this;
 		var foundItems = [];
-		
+
 		service.getMatchedMenuItems = function (searchTerm) {
 			return $http({
 				method: "GET",
 				url: (ApiBasePath + "/menu_items.json")
 			})
-			.then(function (result) {
-				foundItems = [];
-				result.data.menu_items.forEach(function(obj) {
-					if(obj.description.includes(searchTerm)){
-						var tempObj = {
-							name: obj.name,
-							short_name: obj.short_name,
-							description: obj.description
-						};
-						foundItems.push(tempObj); 
-					}
-				});
+			.then(function (result){
+				foundItems = searchTermOnArray(result.data.menu_items, searchTerm);
 				return foundItems;
 			});
 		};
-	  
+
+		service.isEmpty = function(){
+			return (foundItems.length == 0);
+		};
+
 		service.removeItem = function (itemIndex) {
 			foundItems.splice(itemIndex, 1);
 		};
+
+		function searchTermOnArray(arrayOfTerms, searchTerm){
+			if (searchTerm != ""){
+	      return itemsThatMatchedTerm(searchTerm, arrayOfTerms);
+	    }
+	    return [];
+	  }
+
+		function itemsThatMatchedTerm(searchTerm, arrayOfTerms){
+	    var rta = [];
+	    for (var i = 0; i < arrayOfTerms.length; i++) {
+	      if (arrayOfTerms[i].description.includes(searchTerm)){
+	        rta.push(arrayOfTerms[i]);
+	      }
+	    }
+	    return rta;
+	  }
 	}
 })();
