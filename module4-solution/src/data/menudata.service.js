b851d8d629b82a384a58bb7241cb1199bcabf2f3
Gaurav Shah <gauravshah_786@yahoo.com>
2017-02-17 16:46:58 -0600

    Module 4 solution

---
+++ module4-solution/src/data/menudata.service.js
@@ -0,0 +1,27 @@
+(function () {
+'use strict';
+
+angular.module('Data')
+.service('MenuDataService', MenuDataService)
+.constant('ApiBasePath',"https://davids-restaurant.herokuapp.com");
+
+MenuDataService.$inject = ['$http', 'ApiBasePath'];
+function MenuDataService($http,ApiBasePath) {
+    var service = this;
+
+    service.getAllCategories = function(){
+      return $http({
+          method: "GET",
+          url: (ApiBasePath + "/categories.json")
+        });
+    };
+
+    service.getItemsforCategory = function(categoryShortName){
+      var url = ApiBasePath + "/menu_items.json?category=" + categoryShortName;
+      return $http({
+          method: "GET",
+          url: url
+      });
+    };
+  }
+})();
---
+++ module4-solution/src/routes.js
@@ -0,0 +1,60 @@
+(function () {
+'use strict';
+
+angular.module('MenuApp')
+.config(RoutesConfig);
+
+RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+function RoutesConfig($stateProvider, $urlRouterProvider) {
+
+  // Redirect to home page if no other URL matches
+  $urlRouterProvider.otherwise('/');
+
+  // *** Set up UI states ***
+  $stateProvider
+
+  .state('home', {
+    url: '/',
+    templateUrl: 'src/menuapp/templates/home.template.html'
+  })
+
+  .state('categories', {
+    url: '/categories',
+    templateUrl: 'src/menuapp/templates/main-categories.template.html',
+    controller: 'CategoryListController as categoryList',
+    resolve: {
+      categories: ['MenuDataService', function (MenuDataService) {
+        var promise = MenuDataService.getAllCategories();
+        promise.then(function(response){
+            return response.data;
+        })
+        .catch(function(error){
+          console.log(error);
+        });
+      }]
+    }
+  })
+
+  .state('categoryItems', {
+    url: '/items/{categoryId}',
+    templateUrl: 'src/menuapp/templates/main-items.template.html',
+    controller: 'ItemListController as itemList',
+    resolve: {
+      menuItems: ['$stateParams', 'MenuDataService',
+            function ($stateParams, MenuDataService) {
+              return MenuDataService.getAllCategories()
+                .then(function(obj) {
+                  return MenuDataService.getItemsforCategory(obj.data[$stateParams.categoryId].short_name)
+                  .then(function (jsonData){
+                    return jsonData.data.menu_items;
+                  });
+                })
+                .catch(function(error){
+                  console.log(error);
+                });
+            }]
+    }
+  });
+}
+
+})();
